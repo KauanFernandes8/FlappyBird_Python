7f2325f9922efe052fd0a619a1935671b19d8c38
KAUAN GUSTAVO DIAS FERNANDES <cc21243@g.unicamp.br>
2022-11-29 12:35:26 -0300

    avião aparecendo na tela

--- flappy.py
+++ flappy.py
@@ -4,19 +4,38 @@ from pygame.locals import *
 SCREEN_WIDTH = 500
 SCREEN_HEIGHT = 800
 
-class Passaro(pygame.sprite.Sprite):
+class Bird(pygame.sprite.Sprite):
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = pygame.image.load('aviao.jpg').convert_alpha() #recortando a imagem
+        self.rect = self.image.get_rect()
+        print(self.rect)
     
+    def update(self):
+        pass
+
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 FUNDO = pygame.image.load('fundo3.png')#plano de fundo
 FUNDO = pygame.transform.scale(FUNDO,(SCREEN_WIDTH, SCREEN_HEIGHT))#enquadrando a imagem ao tamanho da tela
 
+bird_group =pygame.sprite.Group()
+bird = Bird()
+bird_group.add(bird)
+
 while True:
 
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
+
     screen.blit(FUNDO, (0, 0))
     
+    bird_group.update()
+
+    bird_group.draw(screen)
+
     pygame.display.update()
