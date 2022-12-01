8dbb2191d85108b117ad1fb27b97135dd377d972
KAUAN GUSTAVO DIAS FERNANDES <cc21243@g.unicamp.br>
2022-12-01 09:00:53 -0300

    passarinho batendo asas

--- flappy.py
+++ flappy.py
@@ -1,21 +1,32 @@
+from turtle import speed
 import pygame 
 from pygame.locals import *
 
 SCREEN_WIDTH = 500
 SCREEN_HEIGHT = 800
+SPEED = 1000
 
 class Bird(pygame.sprite.Sprite):
 
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
+        self.MovimentoPassaro = [pygame.image.load('asaCentro.png').convert_alpha(),#vetor que simula o movimento do passaro 
+                                pygame.image.load('asaAlta.png').convert_alpha(), #baseado na troca rapida de imagens
+                                pygame.image.load('asaBaixa.png').convert_alpha()]
 
+        self.atualizaImage = 0 #variavel que controla o "movimento"
         self.image = pygame.image.load('asaCentro.png').convert_alpha() #recortando a imagem
+
         self.rect = self.image.get_rect()
         self.rect[0] = SCREEN_WIDTH/2
         self.rect[1] = SCREEN_HEIGHT/2
     
     def update(self):
-        pass
+        self.atualizaImage = (self.atualizaImage+1) %3#metodo responsavel por fazer acontecer o bater das asas
+        self.image = self.MovimentoPassaro[self.atualizaImage]
+
+        #atualiando altura do passarinho
+        #self.rect[1] += SPEED
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -27,8 +38,10 @@ bird_group =pygame.sprite.Group()
 bird = Bird()
 bird_group.add(bird)
 
-while True:
+clock = pygame.time.Clock() #vai controlar a velocidade do bater de asas
 
+while True:
+    clock.tick()
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
